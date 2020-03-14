c35b37332c4b8769542188a1caaf0eabf8555f1c
raviassis <ravi.g.assis@gmail.com>
2020-03-14 09:35:51 -0300

    add .env file

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -113,7 +113,7 @@
 // Axios
 import axios from 'axios';
 // BUGFIX: same Vue CLI Service URL for CORS with Cue CLI proxy (look at "vue.config.js" file)
-axios.defaults.baseURL = 'http://localhost:4242';
+// axios.defaults.baseURL = 'http://localhost:4242';
 
 export default {
 	name: 'login',
